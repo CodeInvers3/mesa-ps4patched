e2e3479fd46412fe3e74a7dfc5d37f67ade2c5cc
Invers3 <manyajerez@tuta.io>
2025-03-30 22:07:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,30 @@
 meson setup build/ --prefix=/usr --libexecdir=lib --sbindir=bin --auto-features=enabled \
   -Dbuildtype=release \
-  -Doptimization=3 \
-  -Dcpp_std=c++17 \
   -Dplatforms=x11,wayland \
-  -Dgallium-drivers=radeonsi \
+  -Dgallium-drivers=r600,radeonsi \
   -Dvulkan-drivers=amd \
-  -Damd-gpu-targets=gfx601 \
   -Degl=enabled \
   -Dgbm=enabled \
   -Dglx=dri \
-  -Dglvnd=enabled \
   -Dllvm=enabled \
+  -Dshared-llvm=enabled \
+  -Ddraw-use-llvm=true \
+  -Damd-use-llvm=true \
   -Dshared-glapi=enabled \
+  -Dshader-cache=enabled \
   -Dgallium-va=enabled \
   -Dgallium-vdpau=enabled \
-  -Dgallium-xa=disabled \
-  -Dgallium-extra-hud=true \
   -Dgallium-opencl=disabled \
-  -Dgallium-rusticl=false \
-  -Dvideo-codecs=all_free \
+  -Dgallium-xa=disabled \
   -Dvalgrind=disabled \
   -Dlibunwind=disabled \
-  -Dlmsensors=enabled \
+  -Dlmsensors=disabled \
   -Dzstd=enabled \
+  -Dgles1=enabled \
+  -Dgles2=enabled \
   -Dxmlconfig=enabled \
-  -Dosmesa=false \
-  -Dmicrosoft-clc=disabled \
-  -Dandroid-libbacktrace=disabled \
-  -Dpython.bytecompile=1 \
-  -Db_pie=true \
-  -Db_ndebug=true \
-  -Db_lto=true \
-  --wrap-mode=nofallback
-
+  -Dandroid-libbacktrace=disabled
+#Tested Nobara 41
 ninja -C build/
 sudo ninja -C build/ install
 glxinfo | grep "OpenGL version"
